e073e30b59e76c437cd5e8b0965f34dd2ad37410
Zidiny Erre <zidiny.erre.fr@gmail.com>
2025-07-17 21:43:32 +0200

    Mise en place de la partie detection de mouvement

--- index.py
+++ index.py
@@ -1,5 +1,17 @@
-import cv2
-cap  = cv2.VideoCapture(0)
+import cv2 as cv 
+import argparse
+# The OpenCv base was provided by chatgpt
+cap  = cv.VideoCapture(0)
+# argpase act the same as argv agc in C
+parser = argparse.ArgumentParser(description = 'This program enable you to make song with your movement ')
+
+parser.add_argument('--algo', type=str, help= 'Background subtraction method (KNN, MOG2)', default = 'MOG2')
+args = parser.parse_args()
+
+if args.algo == 'MOG2':
+    backSub = cv.createBackgroundSubtractorMOG2()
+else:
+    backSub = cv.createBackgroundSubtractorKNN()
 
 if not cap.isOpened():
     print("Impossible d'accéder à la webcam")
@@ -9,9 +21,14 @@ while True:
     if not ret:
         break
 
-    cv2.imshow("Webcam", frame)  
+    fgMask = backSub.apply(frame)
 
-    if cv2.waitKey(1) == ord('q'):
+    cv.imshow("FG Mask", fgMask)  
+    cv.imshow("Webcam", frame)  
+
+    if cv.countNonZero(fgMask) > 30000:
+        print("wazaaaa")
+    if cv.waitKey(30) == ord('q'):
         break
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
